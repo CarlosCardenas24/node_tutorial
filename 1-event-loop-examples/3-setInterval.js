3f2c5fd83cef0ae0b4d10728bc831a20fdaa0ecb
CarlosCardenas24 <nooptions101@gmail.com>
2023-05-26 21:43:51 -0700

    Hour 3, minute 12

---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    console.log('hello world')
+}, 20000)
+console.log('I will run first')
+// process stays alive unless
+// Kill process CONTROL + C
+// unexpected error
--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-// npm - global command, comes with node
-// npm --version
+const {readFile, writeFile} = require('fs').promises
+/* const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile) */
 
-// local dependancy - use it only in this particular project
-// npm i <packageName>
+const start = async () => {
+    try{
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile(
+            './content/result-mind-grenade.txt', 
+            `THIS IS AWESOME: ${first} and ${second} updated`
+        )
+        console.log(first)
+        console.log(second)
+    } catch (error) {
+        console.log(error)
+    }
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+}
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+start()
 
-const _ = require('lodash')
+/* const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if(err){
+                reject(err)
+            }else {
+                resolve(data)
+            }
+        })
+    })
+} */
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+/* getText('./content/first.txt')
+.then((result) => console.log(result))
+.catch((err) => console.log(err)) */
